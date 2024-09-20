01492e347cf575ca0524fc5e7ca23fc24675c141
sarath <sarath.limenzy@gmail.com>
2024-09-20 14:23:21 +0530

    refactor: make all env variables in uppercase

--- config/database.js
+++ config/database.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 AWS.config.update({
-    accessKeyId: process.env.accessKeyId,
-    secretAccessKey: process.env.secretAccessKey,
-    region: process.env.region,
-    endpoint: process.env.endpoint,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: process.env.REGION,
+    endpoint: process.env.ENDPOINT,
 });
 
 const db = new AWS.DynamoDB.DocumentClient({ convertEmptyValues: true });
